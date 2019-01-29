0b14926554d167a4c08cb06a8c1a47c8537eebfc
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-01-29 14:20:04 +0100

    References in Rust

--- 006-ownership/src/main.rs
+++ 006-ownership/src/main.rs
@@ -5,6 +5,13 @@
 *
 * variables is copy when they are transmitted in arguments function.
 *
+* & chars to references
+*
+* Data race is in situation:
+*
+* Two or more pointers access the same data at the same time.
+* At least one of the pointers is being used to write to the data.
+* There’s no mechanism being used to synchronize access to the data.
 *
 *
 */
@@ -31,6 +38,21 @@ fn main() {
     let (str_var, i8_var) = multi_return();
 
     println!("Function multi_return return string value: \"{}\" and i8 value: {}", str_var, i8_var);
+
+    println!("******************* References *******************");
+
+    let mut string_add_var = String::from("INIT ");
+
+    println!("string_add_var value before func add_string in main function: {}", string_add_var);
+
+    add_string(&mut string_add_var);
+
+    //return "string_add_var value in main function: INIT HELLO WORLD"
+    println!("string_add_var value after func add_string in main function: {}", string_add_var);
+
+    //let s1 = &string_add_var; // no problem
+    //let s2 = &string_add_var; // no problem
+    //let s3 = &mut string_add_var; // Bug
 }
 
 fn copy_fn(s: String) {
@@ -49,4 +71,13 @@ fn return_string() -> String {
 
 fn multi_return() -> (String, i8) {
     ("Hello World".to_string(), 32)
+}
+
+fn add_string(string_add_var: &mut String) {
+// Bug
+// fn add_string(string_add_var: &String) {
+// fn add_string(string_add_var: String) {
+    string_add_var.push_str("HELLO WORLD");
+
+    println!("Value add string is: {}", string_add_var);
 }
